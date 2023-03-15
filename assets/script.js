cb9d0abc6ee0258a6653bf12d68233ef4ad01073
Maxence Badin Léger <maxencelger@gmail.com>
2023-03-15 15:56:01 +0100

    EventListener en cours

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,37 @@
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: "slide1.jpg",
+    tagLine: "Impressions tous formats <span>en boutique et en ligne</span>",
+  },
+  {
+    image: "slide2.jpg",
+    tagLine:
+      "Tirages haute définition grand format <span>pour vos bureaux et events</span>",
+  },
+  {
+    image: "slide3.jpg",
+    tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>",
+  },
+  {
+    image: "slide4.png",
+    tagLine: "Autocollants <span>avec découpe laser sur mesure</span>",
+  },
+];
 
+// VARIABLE
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+
+// METHODE = EVENT
+arrowLeft.addEventListener('click', slidel)
+// FUNCTION
+function slidel(){
+  console.log("hello");
+}
+
+// EVENT
+arrowRight.addEventListener('click', slider)
+// FUNCTION
+function slider(){
+  console.log("hi");
+}
